ef2d24eb786e7e8bc0edb1dfbc535a68ce0d63f8
Clayton Cronk <awesomecronk@gmail.com>
2022-07-23 20:22:09 -0500

    Fixed terrible slowdown. Added several arguments to tune things. Rewrote README.md.

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
+# uPyFile
 This is a file management tool for use with MicroPython (uPython). It works by sending Ctrl+C to the device in order to stop execution of whatever script may be running, then waits for a REPL prompt. It then sends the python commands to open the correct file and read/write it.
-
-## Usage:
-
-`python uPyFile.py port read deviceFileName  -  reads a file from the device and prints its contents to the terminal.`
-
-`python uPyFile.py port pull deviceFileName pcFileName  -  copies a file from the device to your PC.`
-
-`python uPyFile.py port push pcFileName deviceFileName  -  copies a file from your PC to the device.`
-
-Add `-v` or `--verbose` to the end of any command to get a printout of what uPyFile is doing. Please note that if uPyFile says something about an index error when you run it, you are probably missing a command line argument.
-
-## Examples:
-
-`python uPyFile.py COM3 read boot.py`
-
-`python uPyFile.py /dev/ttyUSB0 pull main.py`
 
 Currently, the program is pretty functional. It has a working installer (just download or clone this project and run the installer) and currently supports the following operations:
 
@@ -34,7 +19,7 @@ python3 -m nuitka install.py --onefile -o install
 
 ### Windows:
 ```
-python -m nuitka uPyFile.py --onefile -o uPyFile.exe
+python3 -m nuitka uPyFile.py --onefile -o uPyFile.exe
 python3 -m nuitka install.py --onefile -o install.exe
 ```
 
@@ -42,12 +27,12 @@ python3 -m nuitka install.py --onefile -o install.exe
 Clone the repository or click the green `Code` button and open a terminal in the downloaded folder.
 
 ### Linux:
-Run `./install`, add `C:\Users\%USERNAME%\AppData\Local\Programs\uPyFile` to PATH.
-
-### Windows:
 Run:
 ```
 $ sudo ./install
 $ sudo chmod 775 /usr/local/bin/uPyFile/*
 $ sudo ln -s /usr/local/bin/uPyFile/uPyFile /usr/local/bin/upyfile
 ```
+
+### Windows:
+Run `./install` and add `C:\Users\%USERNAME%\AppData\Local\Programs\uPyFile` to PATH.
--- uPyFile.py
+++ uPyFile.py
@@ -1,12 +1,15 @@
-import serial, argparse
+import argparse, logging, serial
 
 _version = '2.0.0'
+readLimit = 4096
 
 class fileHandler():
     def __init__(self):
         self.debug = True
         self.verbose = True
         self.portOpen = False
+        self.pcLogger = logging.getLogger('PC')
+        self.devLogger = logging.getLogger('DEV')
 
     def __enter__(self):
         return self
@@ -15,28 +18,16 @@ class fileHandler():
         self.close()
 
 
-    # Debug methods (to be replaced with logging calls)
-    def debugComputer(self, debugData):
-        if self.debug:
-            print('COMPUTER: {}'.format(debugData))
-
-    def debugDevice(self, debugData):
-        if self.debug:
-            print('DEVICE: {}'.format(debugData))
-
-    def vbPrint(self, text, end = '\n'):
-        if self.verbose:
-            print(text, end = end)
-
-
     # Serial port management
-    def open(self, port, baud = 115200, timeout = 2, stopBits = 1):
-        self.vbPrint('Opening serial port...', end = ' ')
-        self.serialPort = serial.Serial(port = port,
-                                        baudrate = baud,
-                                        bytesize = 8,
-                                        timeout = timeout,
-                                        stopbits = stopBits)
+    def open(self, port, baud = 115200, timeout = 0.2, stopBits = 1):
+        logging.info('Opening serial port')
+        self.serialPort = serial.Serial(
+            port = port,
+            baudrate = baud,
+            bytesize = 8,
+            timeout = timeout,
+            stopbits = stopBits
+        )
         self.portOpen = True
 
     def waitForREPL(self):
@@ -44,43 +35,47 @@ class fileHandler():
         while(devOutput != b'>>> '):
             if self.serialPort.in_waiting > 0:
                 devOutput = self.serialPort.readline()
-                self.debugDevice(devOutput)
+                self.devLogger.debug(devOutput)
 
     def close(self):
         if self.portOpen:
-            self.vbPrint('Closing serial port...', end = ' ')
+            logging.info('Closing serial port')
             self.serialPort.close()
-            self.vbPrint('Done')
         else:
-            self.vbPrint('Serial port already closed')
+            logging.info('Serial port already closed')
 
 
     # Command implementation
     def init(self, args):
-        self.vbPrint('Done\nRebooting device...', end = ' ')
+        print('Initializing device')
         self.serialPort.write(b'\x03')           #send the stop code
         self.waitForREPL()
-        self.vbPrint('Done.')
 
     def read(self, args, _print = True):
-        self.vbPrint('Sending read command...', end = ' ')
-        cmdText = "fileDev = open('{}', 'rb')\r\nfor i in fileDev.read():\r\nprint(hex(i), end = ' ')\r\n\x7f\r\nfileDev.close()".format(args.file)
+        logging.info('Sending read command')
+        cmdText = '\r\n'.join([
+            "fileDev = open('{}', 'rb')".format(args.file),
+            "for i in fileDev.read(): print(hex(i))",
+            "",
+            "fileDev.close()",
+            ""
+            ])
         dataToSend = bytes(cmdText, 'UTF-8')
         self.serialPort.write(dataToSend)
-        self.debugComputer(dataToSend)
+        self.pcLogger.debug(dataToSend)
 
-        self.vbPrint('Done.\nReading response...')
+        logging.info('Reading response...')
         deviceOutput = b''
-        dataReceived = self.serialPort.read(1024)
+        dataReceived = self.serialPort.read(readLimit)
         while dataReceived:
             deviceOutput += dataReceived
-            dataReceived = self.serialPort.read(1024)
-        self.debugDevice(deviceOutput)
-        self.vbPrint('Done.\nParsing response...', end = ' ')
+            dataReceived = self.serialPort.read(readLimit)
+        self.devLogger.debug(deviceOutput)
+        logging.info('Parsing response')
         data = b''
-        for i in deviceOutput.decode('UTF-8', errors = 'ignore').split('\r\n')[-1][0:-20].split(' '):       #Get a list of bytes in '0xab' format
-            data += int(i, base = 16).to_bytes(1, 'little')     #convert to raw data and add to data variable
-        self.vbPrint('Done.')
+        for i in deviceOutput.decode('UTF-8', errors = 'ignore').split('\r\n')[3:]:       #Get a list of bytes in '0xab' format
+            if i[0:4] == '>>> ': break
+            logging.debug(repr(i)); data += int(i, base = 16).to_bytes(1, 'little')     #convert to raw data and add to data variable
         if _print:
             print('Contents of {} are:'.format(args.file))
             print(data.decode('UTF-8', errors = 'ignore'))      #print text version
@@ -88,40 +83,48 @@ class fileHandler():
             return data
 
     def push(self, args):
-        self.vbPrint('Reading file data on PC...', end = ' ')
+        logging.info('Reading file data on PC')
         with open(args.infile, 'rb') as filePC:
             fileData = str(filePC.read())
             #print('fileData: {}'.format(fileData))
-        self.vbPrint('Done.\nSending write command...')
-        cmdText = "fileDev = open('{}', 'wb')\r\nfileDev.write({})\r\nfileDev.close()\r\n".format(args.outfile, fileData)
+        logging.info('Sending write command...')
+        cmdText = '\r\n'.join([
+            "fileDev = open('{}', 'wb')".format(args.outfile),
+            "fileDev.write({})".format(fileData),
+            "fileDev.close()",
+            ""
+        ])
         dataToSend = bytes(cmdText, 'UTF-8')
         self.serialPort.write(dataToSend)
-        self.debugComputer(dataToSend)
-        self.vbPrint('Done.')
+        self.pcLogger.debug(dataToSend)
         self.waitForREPL()
 
     def pull(self, args):
         data = self.read(args.infile, _print = False)
-        self.vbPrint('Writing data to file...', end = ' ')
+        logging.info('Writing data to file')
         with open(args.outfile, 'wb') as filePC:
             filePC.write(data)
-        self.vbPrint('Done.')
 
     def list(self, args):
-        self.vbPrint('Sending list command...', end = ' ')
-        cmdText = "import os\r\nos.listdir('{}')\r\n".format(args.dir)
+        logging.info('Sending list command')
+        cmdText = '\r\n'.join([
+            "import os",
+            "for item in os.listdir('{}'): print(item)".format(args.dir),
+            "",
+            ""
+        ])
         dataToSend = bytes(cmdText, 'UTF-8')
         self.serialPort.write(dataToSend)
-        self.debugComputer(dataToSend)
-        self.vbPrint('Done.\nReading response...', end = ' ')
-        dataReceived = self.serialPort.read(1024)
-        self.debugDevice(dataReceived)
-        self.vbPrint('Done.\nParsing response...')
+        self.pcLogger.debug(dataToSend)
+        logging.info('Reading response')
+        dataReceived = self.serialPort.read(readLimit)
+        self.devLogger.debug(dataReceived)
+        logging.info('Parsing response')
         listing = ''
-        for i in dataReceived.decode('UTF-8', errors = 'ignore').split('\r\n')[-2][2:-2].split("', '"):
+        for i in dataReceived.decode('UTF-8', errors = 'ignore').split('\r\n')[3:]:
+            if i == '>>> ': break
             listing += i
             listing += '\n'
-        self.vbPrint('Done.')
         print('Contents of directory {} are:'.format(args.dir))
         print(listing)
 
@@ -129,13 +132,28 @@ if __name__ == '__main__':
     with fileHandler() as handler:
         rootParser = argparse.ArgumentParser(prog='uPyFile', description='A file transfer utility for MicroPython devices')
         rootParser.add_argument(
-            'device',
-            help='Device to interact with',
+            '-p',
+            '--port',
+            help='Port the device is on',
             type=str
         )
         rootParser.add_argument(
-            '--verbose',
+            '-b',
+            '--baud',
+            help='Baudrate to use',
+            type=int,
+            default=115200
+        )
+        rootParser.add_argument(
+            '-t',
+            '--timeout',
+            help='Timeout (in seconds) for serial comms',
+            type=float,
+            default=0.2
+        )
+        rootParser.add_argument(
             '-v',
+            '--verbose',
             help='Enable verbose output',
             action='store_true'
         )
@@ -191,18 +209,7 @@ if __name__ == '__main__':
         args = rootParser.parse_args()
 
 
-        handler.open(args.device)   # Make the serial connection
-        args.function(args)         # Run the command
-
-        # if action == 'init':
-        #     handler.initDevice()
-        # elif action == 'read':
-        #     handler.read(sys.argv[3])
-        # elif action == 'push':
-        #     handler.push(sys.argv[3], sys.argv[4])
-        # elif action == 'pull':
-        #     handler.pull(sys.argv[3], sys.argv[4])
-        # elif action == 'ls' or action == 'list':
-        #     handler.list(sys.argv[3])
-        # elif action == 'version':
-        #     print('Using uPyFile version {}'.format(_version))
+        if args.verbose: logging.basicConfig(level=logging.DEBUG)
+        else: logging.basicConfig(level=logging.WARNING)
+        handler.open(args.port, baud=args.baud, timeout=args.timeout)   # Make the serial connection
+        args.function(args) # Run the command
